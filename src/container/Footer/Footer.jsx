9db8bafceee091b16d2ec201edf1cc476df35255
unknown <jamesagu2001@gmail.com>
2024-01-04 07:20:09 -0800

    second commit

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => (
       </div>
     </div>
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
+      <p className="p__opensans">2024 Gerícht. All Rights reserved.</p>
     </div>
   </div>
 );
